1977d9abcad51dd4e898c0eea02fa45e2709ba45
Ana <carmorais14@gmail.com>
2024-05-20 17:01:03 -0300

    Realização do exercício "Smart TV"

--- sintaxe/src/Operadores.java
+++ sintaxe/src/Operadores.java
@@ -3,12 +3,12 @@ import java.sql.Date;
 public class Operadores {
     public static void main(String[] args) {
         //classe Operadores.java
-    String nome = "Ana";
+    string nome = "Ana";
     int idade = 22;
     double peso = 68.5;
     char sexo = 'M';
     boolean doadorOrgao = false;
-    Date dataNascimento = new Date(idade);
+    date dataNascimento = new Date(idade);
 }
     
 }
