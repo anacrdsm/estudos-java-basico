b44b4bff762ba2e4ec1b1ac6f58760b4061904ac
Ana <carmorais14@gmail.com>
2024-05-20 17:01:30 -0300

    Exercício Smart TV

---
+++ sintaxe/src/SmartTv.java
@@ -0,0 +1,33 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void aumentarVolume(){
+        //volume = volume + 1;
+        volume++;
+    }
+
+    public void diminuirVolume(){
+        //volume = volume - 1;
+        volume--;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+
+
+}
---
+++ sintaxe/src/Usuario.java
@@ -0,0 +1,23 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+
+        System.out.println("TV ligada ? " + smartTv.ligada);
+        System.out.println("Canal atual : " + smartTv.canal);
+        System.out.println("Volume atual : " + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+
+        
+    
+
+    }
+}
